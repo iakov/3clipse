982c834249468d437eb0f5a78571a3006f5f7c36
penguiiini <plombiiiri@yandex.ru>
2022-08-07 00:03:11 +0300

    Run to walk transition added

--- Assets/3ClipseGame/Steam/Entities/Player/MainCharacter/MainCharacterStateMachine/Structure/SubStates/ExploreSubStates/ExploreRunSubState.cs
+++ Assets/3ClipseGame/Steam/Entities/Player/MainCharacter/MainCharacterStateMachine/Structure/SubStates/ExploreSubStates/ExploreRunSubState.cs
@@ -45,8 +45,9 @@ namespace _3ClipseGame.Steam.Entities.Player.MainCharacter.MainCharacterStateMac
         public override bool TrySwitchState(out MainCharacterState newMainCharacterState)
         {
             newMainCharacterState = null;
-
-            if (Context.InputHandler.IsJumpPressed) newMainCharacterState = _factory.Jump();
+            
+            if (Context.Stamina.StaminaValue == 0) newMainCharacterState = _factory.Walk();
+            else if (Context.InputHandler.IsJumpPressed) newMainCharacterState = _factory.Jump();
             else if (!Context.PlayerController.IsGrounded && !Physics.Raycast(Context.Transform.position, Vector3.down,
                     Context.PlayerController.Radius)) newMainCharacterState = _factory.Fall();
             else if (Context.InputHandler.CurrentInput == Vector2.zero) newMainCharacterState = _factory.Stop();
